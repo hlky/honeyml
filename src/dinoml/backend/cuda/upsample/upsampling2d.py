e44d43dc98b0c4c6c2f491e12bbfa94d769bd8c9
hlky <hlky@hlky.ac>
2026-01-01 19:18:14 +0000

    Static Upsampling 2d kernel

--- src/dinoml/backend/cuda/upsample/upsampling2d.py
+++ src/dinoml/backend/cuda/upsample/upsampling2d.py
@@ -20,18 +20,6 @@ from dinoml.backend import registry
 from dinoml.backend.backend_spec import CUDASpec
 from dinoml.backend.common import upsampling2d_common
 
-# pylint: disable=C0103,C0415,W0613,C0301,W0612
-
-
-Header_Files = """
-#include <cuda_bf16.h>
-#include <cuda_fp16.h>
-#include <cuda_runtime.h>
-#include "cutlass/util/host_tensor.h"
-
-using bfloat16 = __nv_bfloat16;
-"""
-
 
 @registry.reg("cuda.upsampling2d.gen_function")
 def gen_function(
@@ -41,54 +29,13 @@ def gen_function(
     shape_eval_template,
     shape_save_template,
 ):
-    func_name = func_attrs["name"]
-    exec_path = func_attrs["exec_path"]
-    x = func_attrs["inputs"][0]
-    backend_spec = CUDASpec()
-    input_type = backend_spec.dtype_to_backend_type(x._attrs["dtype"])
-    half2_data_ref = backend_spec.half2_data_ref
-
-    args = {
-        "indent": "    ",
-        "dtype": "int64_t ",
-        "div": "/",
-        "x_dim0": "*batch",
-        "x_dim1": "*in_h",
-        "x_dim2": "*in_w",
-        "x_dim3": "*in_ch",
-    }
-    if func_attrs["out_shape"] is True:
-        args["out_h"] = "*out_h"
-        args["out_w"] = "*out_w"
-    else:
-        args["scale_factor"] = func_attrs["scale_factor"]
-    shape_eval_func = shape_eval_template.render(
-        **args,
-    )
-    shape_save_func = shape_save_template.render(
-        indent="  ",
-        y_dim0="*out_batch",
-        y_dim1="*out_h",
-        y_dim2="*out_w",
-    )
-    shape_func = shape_eval_func + shape_save_func
-    exec_paths = ""
-    for key in exec_path:
-        program = upsampling2d_common.EXEC_TEMPLATE.render(dtype=input_type)
-        exec_inst = exec_cond_template.render(indent="  ", cond=key, program=program)
-        exec_paths += exec_inst
-    return upsampling2d_common.SRC_TEMPLATE.render(
-        header_files=Header_Files,
-        function_name=func_name,
-        shape_function=shape_func,
-        exec_paths=exec_paths,
-        index_type=backend_spec.index_type,
-        prefix=backend_spec.prefix,
-        half2_data_ref=half2_data_ref,
-        mode=func_attrs["mode"],
-        tsize=upsampling2d_common.gen_alignment(x),
-        align_corners=func_attrs["align_corners"],
-        dtype=input_type,
+    return upsampling2d_common.gen_function(
+        func_attrs,
+        template_path,
+        exec_cond_template,
+        shape_eval_template,
+        shape_save_template,
+        backend_spec=CUDASpec(),
     )
 
 
